4be67e74f0e77b695dc00cf0cb31deb11f888224
kaushikkodarapu <kaushikkodarapu1@gmail.com>
2024-08-12 21:37:28 +0530

    first committ

--- flashcard-backend/index.js
+++ flashcard-backend/index.js
@@ -3,16 +3,21 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'kaushik',
-  password: 'kaushik',
-  database: 'flashcards_db'
+//   host: 'localhost',
+//   user: 'kaushik',
+//   password: 'kaushik',
+//   database: 'flashcards_db'
+
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 db.connect(err => {
